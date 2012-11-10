9be4ba18cb27c6131c406ff6e473b458dcf034d5
Bryan Ngo <bngo92@gmail.com>
2012-11-09 23:30:46 -0800

    Added basic html form, basic ticket class, and form submission

--- main.py
+++ main.py
@@ -15,11 +15,60 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import urllib
+from google.appengine.ext import db
+
+class Ticket(db.Model):
+    email = db.EmailProperty()
+    ticket_number = db.StringProperty()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('<html><body>')
+        tickets = db.GqlQuery("SELECT * "
+                "FROM Ticket ")
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+        if not tickets:
+            return
+
+        self.response.write('<table>')
+        for ticket in tickets:
+            self.response.out.write(
+                    '<tr><td>%s</td><td>%s</td></tr>' % 
+                    (ticket.email, ticket.ticket_number))
+        self.response.write('</table>')
+
+        self.response.out.write("""
+        <form action="/add" method="post">
+        Background picture: <input type="file" name="picture">
+        <hr>
+        Custom text blurb:
+        </br>
+        Top: <input name="blurb_top">
+        </br>
+        Mid: <input name="blurb_mid">
+        </br>
+        Bot: <input name="blurb_bot">
+        <hr>
+        Ticket category: <input name="ticket_category">
+        <hr>
+        Ticket number format: <input name="ticket_format">
+        <hr>
+        Number of tickets to be generated: <input name="num_tickets">
+        <hr>
+        Email address to which tickets should be sent: <input name="email">
+        <input type="submit" value="Submit"></form>
+        </body>
+        </html>""")
+
+class Add(webapp2.RequestHandler):
+    def post(self):
+        email = self.request.get('email')
+        ticket_number = self.request.get('ticket_number')
+        Ticket(email=email, ticket_number=ticket_number).put()
+        self.redirect('/')
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+    ('/add', Add)], 
+    debug=True)
